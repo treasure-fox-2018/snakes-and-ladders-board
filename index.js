b7237ada58bc78e5366e0129b8dffe5ed87d9b27
Ari Supriatna <arisupriatna@fabio-Aspire-E5-476G.localdomain>
2018-05-30 19:47:34 +0700

    sneakLadders done

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-function sneakLadders(snake) {
-  let angka = snake * snake
-  let board = []
-  let reverse = []
-  for (var i = 0; i < snake; i++) {
-    board.push([])
-    for (var j = 0; j < snake; j++) {
-      board[i].push(angka--)
+function sneakLadders(input) {
+  let startNumber = Math.pow(input, 2)
+  let endNumber = startNumber - input + 1
+  let gameBoard = []
+
+  if (input % 2 === 0) {
+
+    for (let i = 0; i < input; i++) {
+      gameBoard.push([])
+
+      if (i % 2 === 0) {
+        for (let a = startNumber; a >= endNumber; a--) {
+          gameBoard[i].push(a)
+        }
+      } else {
+        for (let a = endNumber; a <= startNumber; a++) {
+          gameBoard[i].push(a)
+        }
+      }
+
+      startNumber -= input
+      endNumber -= input
     }
+
+  } else {
+
+    for (let j = 0; j < input; j++) {
+      gameBoard.push([])
+
+      if (j % 2 === 1) {
+        for (let a = startNumber; a >= endNumber; a--) {
+          gameBoard[j].push(a)
+        }
+      } else {
+        for (let a = endNumber; a <= startNumber; a++) {
+          gameBoard[j].push(a)
+        }
+      }
+
+      startNumber -= input
+      endNumber -= input
+    }
+
   }
 
-  //mereverse index kelipatan 2
-  var hasilReverse = []
-  for (var i = board.length - 1; i >= 0; i -= 2) {
-    for (var j = board[i].length-1; j >= 0; j--) {
-      reverse.push(board[i][j])
-    }
-    if (reverse.length === snake) {
-      hasilReverse.push(reverse)
-      reverse = []
-    }
-  }
-
-  return hasilReverse
-
-  for (var a = board.length-1; a >= 0; a -= 2) {
-    // console.log(board[a])
-  }
-
-  // return board
-
-
-
-
-
-
+  return gameBoard
 }
-// console.log(sneakLadders(10))
-// console.log(sneakLadders(15))
+console.log(sneakLadders(10))
+console.log(sneakLadders(15))
 console.log(sneakLadders(4))
